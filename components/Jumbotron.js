d071d3ae246f5c661d67c29374f4672f54c9430d
Bob F <bobfdev@gmail.com>
2022-01-25 21:01:39 -0600

    initial files

---
+++ components/Jumbotron.js
@@ -0,0 +1,13 @@
+
+
+export default function Jumbotron() {
+  return (
+    <div className="p-5 mb-4 bg-light rounded-3 mt-5">
+        <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bold">Pro Sports</h1>
+            <p className="col-md-8 fs-4">The highest level of professional sports leagues across the world.</p>
+            <button className="btn btn-primary btn-lg" type="button">Example button</button>
+        </div>
+    </div>
+);
+}
